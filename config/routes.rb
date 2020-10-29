ea829a32f937938f25a23db7007281fa2edfca3b
Christopher Amanor <kriskit7@gmail.com>
2020-10-29 20:11:59 +0000

    Add model and controller for vehicles

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
     resource :users, only: [:create]
     post "/login", to: "users#login"
     get "/auto_login", to: "users#auto_login"
+    resources :vehicles
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_29_184720) do
+ActiveRecord::Schema.define(version: 2020_10_29_200411) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,6 +18,15 @@ ActiveRecord::Schema.define(version: 2020_10_29_184720) do
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "password_digest"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "vehicles", force: :cascade do |t|
+    t.string "name"
+    t.string "model"
+    t.string "transmission"
+    t.string "fee"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
