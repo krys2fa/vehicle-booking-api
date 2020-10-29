1744f1c44f333e4d8fd21c4efd73c7b986aa1404
Christopher Amanor <kriskit7@gmail.com>
2020-10-29 20:26:58 +0000

    Add model and controller for appointments

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_29_200411) do
+ActiveRecord::Schema.define(version: 2020_10_29_202119) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "appointments", force: :cascade do |t|
+    t.date "date"
+    t.string "city"
+    t.string "vehicle"
+    t.string "model"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_appointments_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
@@ -31,4 +42,5 @@ ActiveRecord::Schema.define(version: 2020_10_29_200411) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "appointments", "users"
 end
