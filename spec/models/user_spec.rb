3393c711b0d5efd5353fe5c23073817724b4d1b2
Christopher Amanor <kriskit7@gmail.com>
2020-11-06 15:46:15 +0000

    Add more tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,6 +5,7 @@ RSpec.describe User, :type => :model do
 	describe 'validations' do
     it { should validate_presence_of(:username) }
     it { should validate_presence_of(:password) }
+    it { should have_secure_password }
   end
 
 	describe 'associations' do
