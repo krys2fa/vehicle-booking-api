4708a8b7ad0aa4965b993cfc22d384b2825ec9d9
Christopher Amanor <kriskit7@gmail.com>
2020-11-09 10:17:40 +0000

    Fix linter errors

--- spec/models/appointment_spec.rb
+++ spec/models/appointment_spec.rb
@@ -1,17 +1,15 @@
 require 'rails_helper'
 
-RSpec.describe Appointment, :type => :model do
-
-	describe 'validations' do
+RSpec.describe Appointment, type: :model do
+  describe 'validations' do
     it { should validate_presence_of(:date) }
     it { should validate_presence_of(:city) }
-		it { should validate_presence_of(:vehicle) }
+    it { should validate_presence_of(:vehicle) }
     it { should validate_presence_of(:model) }
-		it { should validate_presence_of(:user_id) }
+    it { should validate_presence_of(:user_id) }
   end
 
-	describe 'associations' do
+  describe 'associations' do
     it { should belong_to(:user) }
   end
-
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,15 +1,13 @@
 require 'rails_helper'
 
-RSpec.describe User, :type => :model do
-
-	describe 'validations' do
+RSpec.describe User, type: :model do
+  describe 'validations' do
     it { should validate_presence_of(:username) }
     it { should validate_presence_of(:password) }
     it { should have_secure_password }
   end
 
-	describe 'associations' do
+  describe 'associations' do
     it { should have_many(:appointments) }
   end
-
 end
