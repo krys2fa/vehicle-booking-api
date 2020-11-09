60e8ea5af490c1fba5e5555a484e5c1c0a9b5348
Christopher Amanor <kriskit7@gmail.com>
2020-11-09 10:01:31 +0000

    Add photo column to vehicles model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_29_202119) do
+ActiveRecord::Schema.define(version: 2020_11_09_095939) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,6 +40,7 @@ ActiveRecord::Schema.define(version: 2020_10_29_202119) do
     t.string "fee"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "photo"
   end
 
   add_foreign_key "appointments", "users"
